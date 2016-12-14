5565619502d53a90464004f506281d854711b3dd
Afidah S <fiefed@yahoo.com>
2016-12-14 16:56:11 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	/*here put jquery executions*/
+	
+});
---
+++ js/form.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	/*here put jquery executions*/
+	 jQuery('.foxform').foxholder({
+    demo: 10 //or other number of demo (1-15) you want to use
+  });
+});
